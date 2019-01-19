83afabb95a708e8b16a53aeb3e4438da36fd9484
hydra13 <rob.sabirov@yandex.ru>
2019-01-19 15:56:02 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Radio Player
+Description
+-----------
+A simple radio player. Used JavaScript, [Howled.js](https://howlerjs.com/) and [Bootstrap](https://getbootstrap.com/).
---
+++ js/stations.js
@@ -0,0 +1,237 @@
+let stations = [{
+        name: 'Hip Hop Hits 89.9',
+        url: 'http://tunein4.streamguys1.com/hhbeafree5'
+    },
+    {
+        name: "Today's Hits 107.7",
+        url: 'http://rfcmedia.streamguys1.com/MusicPulse.mp3'
+    },
+    {
+        name: '100hitz - Hot Hitz',
+        url: 'http://gr01.cdnstream.com:8290/'
+    },
+    {
+        name: 'KFI AM 640',
+        url: 'http://c8icyelb.prod.playlists.ihrhls.com/177_icy'
+    },
+    {
+        name: "That 70's Channel",
+        url: 'http://los.cdnstream.com/1101_128?cb=553828.mp3'
+    }, {
+        name: 'K-LOVE Radio ',
+        url: 'http://emf.streamguys1.com/sk005_mp3_high_web'
+    }, {
+        name: 'American Road Radio',
+        url: 'http://s4.radioboss.fm:8101/stream'
+    }, {
+        name: 'Smack Urban Radio',
+        url: 'http://shannon1.serverroom.net:9602/'
+    }, {
+        name: 'Dash Radio',
+        url: 'http://ice55.securenetsystems.net/DASH16'
+    }, {
+        name: 'SmoothJazz.com',
+        url: 'http://sj128.hnux.com/'
+    }, {
+        name: 'KNAC.COM',
+        url: 'http://s5.voscast.com:7346/;'
+    }, {
+        name: '4U Classic Rock',
+        url: 'http://str4uice.streamakaci.com/4uclassicrock.mp3'
+    }, {
+        name: '95.5 KLOS',
+        url: 'http://14643.live.streamtheworld.com/KLOSFMAAC.aac'
+    }, {
+        name: 'Hits101 Radio',
+        url: 'http://173.249.0.141:8226/1'
+    }, {
+        name: 'Hard Rock Heaven',
+        url: 'http://hydra.cdnstream.com/1521_128'
+    }, {
+        name: 'WILD ROCK RADIO',
+        url: 'http://64.150.176.87:8287/;'
+    }, {
+        name: '99.7 Classic Rock',
+        url: 'http://65.19.131.145/momentum-kioofmaac-ibc2'
+    }, {
+        name: 'AAZo Radio - Rap Hip Hop',
+        url: 'http://77.67.46.74:8000/aazoradio-raphiphop.mp3/;'
+    }, {
+        name: 'xxxRock.fm',
+        url: 'http://sc15.shoutcaststreaming.us:8140/;stream.nsv'
+    }, {
+        name: 'Deeper Shades of House',
+        url: 'http://andromeda.housejunkie.ca:8000/radio.mp3'
+    }, {
+        name: 'McQsJazz.com',
+        url: 'http://64.150.176.9:8455/;'
+    }, {
+        name: 'Dash Radio - Love Songs',
+        url: 'http://ice55.securenetsystems.net/DASH66'
+    }, {
+        name: '101 SMOOTH JAZZ',
+        url: 'http://www.vip-radios.fm/stream/101-Smooth-Jazz'
+    }, {
+        name: '790 KABC-AM',
+        url: 'http://15363.live.streamtheworld.com:3690/KABCAM_SC'
+    }, {
+        name: 'The Beach',
+        url: 'http://208.80.54.47/KCDUFM_SC'
+    }, {
+        name: 'SmoothLounge.com',
+        url: 'http://sl128.hnux.com/;'
+    }, {
+        name: 'Rhythm 105.9',
+        url: 'http://64.150.176.192:8062/;stream.mp3'
+    }, {
+        name: 'Slow Jams',
+        url: 'http://c10icyelb.prod.playlists.ihrhls.com/4719_icy'
+    }, {
+        name: 'Hot Talk KSFO 560 AM',
+        url: 'http://18503.live.streamtheworld.com/KSFOAMAAC.aac'
+    }, {
+        name: 'Hot 103.5 FM',
+        url: 'http://18573.live.streamtheworld.com/KHHMFMAAC.aac'
+    }, {
+        name: 'Max FM',
+        url: 'http://16693.live.streamtheworld.com:3690/XPRSFMAAC_SC'
+    }, {
+        name: 'BestNetRadio - The Mix',
+        url: 'http://bigrradio.cdnstream1.com/5169_128'
+    }, {
+        name: '1000 HITS 80s',
+        url: 'http://listen.shoutcast.com/1000hits80s'
+    }, {
+        name: 'MICHAEL JACKSON RADIO',
+        url: 'http://94.23.15.114:8006/;stream.nsv'
+    }, {
+        name: 'Soft Rock Classics',
+        url: 'http://hestia.cdnstream.com/1562_96'
+    }, {
+        name: 'Radio Free 102.3',
+        url: 'http://54.190.183.239/kjlh-kjlhfmaac-ibc2'
+    }, {
+        name: 'ESPN Los Angeles',
+        url: 'http://edge.espn.cdn.abacast.net/espn-kspnmp3-48?source=tunein'
+    }, {
+        name: 'Air1 Radio',
+        url: 'http://emf.streamguys1.com/sa001_mp3_high_tunein'
+    }, {
+        name: 'LA Talk Radio - Channel 1',
+        url: 'http://fmt01.egihosting.com:21700/;stream/1'
+    }, {
+        name: 'Mobb Report Radio',
+        url: 'https://streamer.radio.co/s7a616116e/listen'
+    }, {
+        name: 'Now 100.5',
+        url: 'http://17853.live.streamtheworld.com/KZZOFMAACHI.aac'
+    }, {
+        name: 'Bygolly Old Time Radio',
+        url: 'http://192.240.102.198:14469/stream'
+    }, {
+        name: 'Dash Radio - Doggystyle',
+        url: 'http://ice55.securenetsystems.net/DASH78'
+    }, {
+        name: '100.7 KHAY California Country',
+        url: 'http://18613.live.streamtheworld.com:80/KHAYFMAAC_SC'
+    }, {
+        name: 'Movie Ticket Radio',
+        url: 'http://listen.djcmedia.com/movieticketclassichigh'
+    }, {
+        name: 'Dubstep FM',
+        url: 'http://stream.dubstep.fm/256mp3'
+    }, {
+        name: 'Radio Paradise',
+        url: 'http://stream.radioparadise.com/mp3-192'
+    }, {
+        name: '113.FM BPM RADIO',
+        url: 'http://113fm.cdnstream1.com/1736_128?cb=139271.mp3'
+    }, {
+        name: 'Magic 92.5 FM',
+        url: 'http://18393.live.streamtheworld.com/XHRMFMAAC_SC'
+    }, {
+        name: 'KQED-FM',
+        url: 'http://kqed.ice.lbdns-streamguys.com/kqedradio'
+    }, {
+        name: 'The Independent FM',
+        url: 'http://50.7.98.106:8635/;'
+    }, {
+        name: 'KPFK 90.7 FM',
+        url: 'http://ic1.mainstreamnetwork.com/kpfk-fm.mp3'
+    }, {
+        name: 'KGO 810 AM',
+        url: 'http://18593.live.streamtheworld.com/KGOAMAAC.aac'
+    }, {
+        name: '1045 The Vault',
+        url: 'http://s7.nexuscast.com:8026/;'
+    }, {
+        name: 'Proton Radio',
+        url: 'https://shoutcast.protonradio.com/;'
+    }, {
+        name: 'KCRW',
+        url: 'http://kcrw.streamguys1.com/kcrw_192k_mp3_on_air'
+    }, {
+        name: 'Q97.1',
+        url: 'http://208.80.54.72/KSEQFM_SC'
+    }, {
+        name: 'Kola 99.9FM',
+        url: 'http://18443.live.streamtheworld.com:80/KOLAFMAAC_SC'
+    }, {
+        name: 'FOX News Talk',
+        url: 'http://fnradio-shoutcast-32.ng.akacast.akamaistream.net/7/115/13873/v1/auth.akacast.akamaistream.net/fnradio-shoutcast-32'
+    }, {
+        name: 'Greatest Hits Radio',
+        url: 'http://streaming.live365.com/a22076'
+    }, {
+        name: 'Smooth Jazz Mix New York',
+        url: 'http://hestia2.cdnstream.com/1078_128?cb=696226.mp3'
+    }, {
+        name: 'Side Street Radio',
+        url: 'http://www.ophanim.net:9780/;stream.nsv'
+    }, {
+        name: '95.5 PLJ',
+        url: 'http://playerservices.streamtheworld.com/api/livestream-redirect/WPLJFMAAC.aac?dist=onlineradiobox'
+    }, {
+        name: 'USA Dance Radio',
+        url: 'http://s7.streammonster.com:8260/;stream.nsv'
+    }, {
+        name: 'WNYC Radio',
+        url: 'https://am820.wnyc.org/wnycam-web'
+    }, {
+        name: 'Bloomberg Radio',
+        url: 'https://18843.live.streamtheworld.com/WBBRAMAAC48/HLS/playlist.m3u8'
+    }, {
+        name: 'Hot 108 JAMZ',
+        url: 'http://sc.hot108.com:4000/;'
+    }, {
+        name: 'ESPN New York',
+        url: 'http://edge.espn.cdn.abacast.net/espn-wepnmp3-32?source=tunein'
+    }, {
+        name: 'One Love Hip Hop Radio',
+        url: 'http://listen.one.hiphop/live.mp3'
+    }, {
+        name: '77 WABC Radio',
+        url: 'http://playerservices.streamtheworld.com/api/livestream-redirect/WABCAMAAC.aac?dist=onlineradiobox'
+    }, {
+        name: 'SmoothJazz.NYC',
+        url: 'http://airspectrum.cdnstream1.com:8078/1614_192'
+    }, {
+        name: 'NYC House Radio',
+        url: 'http://www.ophanim.net:8400/;'
+    }, {
+        name: 'Hip Hop Dugout Radio',
+        url: 'http://172.93.51.179:8000/stream/;'
+    }, {
+        name: 'Radio Symphony',
+        url: 'https://streaming.radiostreamlive.com/radiosymphony_devices'
+    }, {
+        name: 'Black Soul Rhythms',
+        url: 'http://playerservices.streamtheworld.com/api/livestream-redirect/SAM08AAC039.mp3?dist=onlineradiobox'
+    }, {
+        name: 'The Blaze Radio',
+        url: 'http://14643.live.streamtheworld.com/BLZE_1.mp3'
+    }, {
+        name: 'AmazingLiteMusic.com',
+        url: 'http://edge1-a.exa.live365.net/a79966_live365'
+    }
+]
